8c33c3ac0787b1d4f2c67db595d3e9275042ca2f
mramsath <mramshadmane@gmail.com>
2026-02-27 17:12:27 +0300

    web viewer issue fixes

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // Use legacy "main" resolution so some packages (e.g. pretty-format) resolve correctly.
-config.resolver = config.resolver || {};
-config.resolver.unstable_enablePackageExports = false;
+// Add additional resolver for React Native
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs'];
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  'react-native': require.resolve('react-native'),
+};
+
+// Add support for Hermes
+config.transformer.unstable_allowRequireContext = true;
 
 module.exports = config;
+
