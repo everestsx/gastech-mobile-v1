2c451adab76da72859bd8933c6f982195f8b74b5
abdulla615 <moulanasyd108@gmail.com>
2026-08-02 19:48:31 +0530

    git bnach issues

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,11 @@
 declare module '@env' {
   export const API: string;
+  export const ODOO_URL: string;
+  export const ODOO_DB: string;
+  export const ODOO_API_KEY: string;
+  export const UID: string;
+  export const GOOGLE_CLIENT_ID: string;
+  export const GOOGLE_CLIENT_SECRET: string;
+  export const GOOGLE_REFRESH_TOKEN: string;
+  export const ROOT_FOLDER_ID: string;
 }
